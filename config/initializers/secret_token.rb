1abebe78fc348ae103d9b375c9926a03239fc91e
Liu Wei <wliu272@163.com>
2013-12-11 22:50:25 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ParheliaPassWeb::Application.config.secret_key_base = 'eb70af408da604a72ffa779f0edb71da0ef1de2b0bf30a76937a8ea64998a089db415ff301dd7d3851b2d8601e50090f73c48e09b5937342bba605b52cf41ee9'
