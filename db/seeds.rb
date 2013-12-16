03996166457e703981898de76395c3570fd9b8cc
Liu Wei <wliu272@163.com>
2013-12-16 10:41:27 +0800

    seeds.rb created for easy filling dummy test data into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+1.upto(100) do
+	k = "taobao#{rand(1..100)}"
+	o = "Liu Wei-#{rand(1..100)}"
+	u = "haha#{rand(100..300)}"
+	p = "haha#{rand(300..500)}"
+	cat = "category#{rand(1..100)}"
+	com = "comment#{rand(1..100)}"
+	Entry.create(:key => k, :owner => o, :user => u, :password => p, :category => cat, :comments => com)
+end
